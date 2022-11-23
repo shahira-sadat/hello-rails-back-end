e475bc7e766c561a86fb9f4691e2e8fbb3988274
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-23 07:32:12 +0430

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
 Greeting.create(text: 'Assalaam Alaikum')
 Greeting.create(text: 'hello')
 Greeting.create(text: 'hi')
