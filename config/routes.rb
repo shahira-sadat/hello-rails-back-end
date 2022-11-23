952286e558878e9b0a393db19c0248f5fa29e2d0
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-23 07:35:39 +0430

    Add controller action and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :greetings
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'api/v1/greeting', to: 'application#greeting'
+  root 'api/v1/application#greeting'
 end
